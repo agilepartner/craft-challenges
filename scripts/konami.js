d23016ffc8368614cfb33be03f9e641c1e835797
Yoan THIRION <ythirion@agilepartner.net>
2018-08-21 14:29:30 +0200

    Remove gesture

--- scripts/konami.js
+++ scripts/konami.js
@@ -14,12 +14,10 @@ class Konami {
         this.keyPosition = 0;
         this.isActivated = false;
         this.konami = this;
-        this.touchConfiguration = new TouchConfiguration();
     }
     
     load() {
         this.startKeyDetection(e => konami.onKeyPressed(e));
-        this.startTouchDetection(e => konami.validateKey(e));
     }
 
     startKeyDetection(onKeyPressed) {
@@ -36,8 +34,7 @@ class Konami {
     validateKey(key){
         var requiredKey = this.konamiCode[this.keyPosition];
 
-        if (key == requiredKey || 
-            ((requiredKey == 'a' || requiredKey == 'b') && key == 'tap')) {
+        if (key == requiredKey) {
             this.keyPosition++;
 
             if (this.keyPosition == this.konamiCode.length) {
@@ -63,64 +60,4 @@ class Konami {
             this.isActivated = false;
         }
     }
-
-    startTouchDetection(onTouch){
-        document.addEventListener('touchstart', function(e){
-            konami.onTouchStart(e);
-        }, false);
-      
-        document.addEventListener('touchend', function(e){
-            konami.onTouchEnd(e, onTouch);
-        }, false)
-
-        document.addEventListener('touchmove', function(e){
-            e.preventDefault();
-        }, false)
-      
-    }
-
-    onTouchStart(e) {
-        var touch = e.changedTouches[0];
-        var touchEvent = new TouchEvent();
-        touchEvent.X = touch.pageX;
-        touchEvent.Y = touch.pageY;
-        touchEvent.time = new Date().getTime();
-        konami.lastTouchEvent = touchEvent;
-    }
-
-    onTouchEnd(e, onTouch){
-        var touch = e.changedTouches[0];
-        var distX = touch.pageX - konami.lastTouchEvent.X;
-        var distY = touch.pageY - konami.lastTouchEvent.Y;
-        var elapsedTime = new Date().getTime() - konami.lastTouchEvent.time;
-        var touchGesture = 'tap';
-
-        if (elapsedTime <= konami.touchConfiguration.allowedTime){
-            if (Math.abs(distX) >= konami.touchConfiguration.threshold && 
-                Math.abs(distY) <= konami.touchConfiguration.restraint){ 
-                touchGesture = (distX < 0)? 'left' : 'right';
-            }
-            else if (Math.abs(distY) >= konami.touchConfiguration.threshold && 
-                Math.abs(distX) <= konami.touchConfiguration.restraint){
-                touchGesture = (distY < 0)? 'up' : 'down';
-            }
-        }
-        onTouch(touchGesture);
-    }
 };
-
-class TouchConfiguration {
-    constructor(){
-        this.threshold = 50;
-        this.restraint = 100;
-        this.allowedTime = 300;
-    }
-}
-
-class TouchEvent {
-    constructor(){
-        this.X = 0;
-        this.Y = 0;
-        this.time = 0;
-    }
-}
